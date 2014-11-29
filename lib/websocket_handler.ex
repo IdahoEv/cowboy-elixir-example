57c5eba8b202b6051a2a758315c0c3c666c912b4
Evan Dorn <evan@lrdesign.com>
2014-11-29 00:25:43 -0800

    assorted cleanup & consistency

--- lib/websocket_handler.ex
+++ lib/websocket_handler.ex
@@ -11,14 +11,16 @@ defmodule WebsocketHandler do
     {:ok, req, :undefined_state }
   end
 
+  def websocket_terminate(_reason, _req, _state) do
+    :ok
+  end
+
   def websocket_handle({:text, content}, req, state) do
-    { :ok, body } = JSEX.decode(content)
-    %{ "message" => message } = body
-    IO.puts "Received message: #{message}"
+    { :ok, %{ "message" => message} } = JSEX.decode(content)
     rev = String.reverse(message)
-    IO.puts "Reversed message: #{rev}"
-    { :ok, reply } = JSEX.encode(%{ "reply" => rev})
-    IO.puts "Replying with: #{inspect(reply)}"
+    { :ok, reply } = JSEX.encode(%{ reply: rev})
+
+    #IO.puts("Message: #{message}")
     {:reply, {:text, reply}, req, state}
   end
   
@@ -26,15 +28,12 @@ defmodule WebsocketHandler do
     {:ok, req, state}
   end
 
-  def websocket_terminate(_reason, _req, _state) do
-    :ok
-  end
-
   def websocket_info({timeout, _ref, _foo}, req, state) do
     {hh,mm,ss} = :erlang.time()
     time = "#{hh}:#{mm}:#{ss}"
-    message = "{ \"time\": \"#{time}\" }"
-    IO.puts("Message: #{message}")
+    { :ok, message } = JSEX.encode(%{ time: time})
+
+    #IO.puts("Message: #{message}")
     :erlang.start_timer(1000, self(), [])
     { :reply, {:text, message}, req, state}
   end
